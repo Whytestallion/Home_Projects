ab09b6fde83faba3e7661f91624d68b0c02f4d28
Whytestallion <Smash.warrior@gmail.com>
2016-04-15 11:18:32 -0500

    Added java folder

---
+++ Java/Hello_World/src/hello_world/Hello_World.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello_world;
+
+/**
+ *
+ * @author Scott Webb
+ */
+public class Hello_World {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    //does basic console output. used to initialize java folder for git
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello world");
+    }
+    
+}
